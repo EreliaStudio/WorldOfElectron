cb8f3d5244c0937a7944c1d03c8132b0553dcb1d
Jeremy Boissy <jeremy.boissy@mediasofts.fr>
2024-12-10 17:32:04 +0100

    Working on making the mesh renderer work

--- src/main.cpp
+++ src/main.cpp
@@ -33,48 +33,48 @@ public:
         _mesh.addFace(
             spk::Mesh::VertexIndex(0, 0),
             spk::Mesh::VertexIndex(1, 1),
-            spk::Mesh::VertexIndex(3, 3),
-            spk::Mesh::VertexIndex(2, 2)
+            spk::Mesh::VertexIndex(2, 2),
+            spk::Mesh::VertexIndex(3, 3)
         );
 
-        // Back face (z = +0.5)
+        // // Back face (z = +0.5)
         _mesh.addFace(
             spk::Mesh::VertexIndex(4, 0),
             spk::Mesh::VertexIndex(6, 1),
-            spk::Mesh::VertexIndex(7, 3),
-            spk::Mesh::VertexIndex(5, 2)
+            spk::Mesh::VertexIndex(5, 2),
+            spk::Mesh::VertexIndex(7, 3)
         );
 
-        // Left face (x = -0.5)
+        // // Left face (x = -0.5)
         _mesh.addFace(
             spk::Mesh::VertexIndex(0, 0),
             spk::Mesh::VertexIndex(2, 1),
-            spk::Mesh::VertexIndex(6, 3),
-            spk::Mesh::VertexIndex(4, 2)
+            spk::Mesh::VertexIndex(4, 2),
+            spk::Mesh::VertexIndex(6, 3)
         );
 
-        // Right face (x = +0.5)
+        // // Right face (x = +0.5)
         _mesh.addFace(
             spk::Mesh::VertexIndex(1, 0),
             spk::Mesh::VertexIndex(5, 1),
-            spk::Mesh::VertexIndex(7, 3),
-            spk::Mesh::VertexIndex(3, 2)
+            spk::Mesh::VertexIndex(3, 2),
+            spk::Mesh::VertexIndex(7, 3)
         );
 
-        // Top face (y = +0.5)
+        // // Top face (y = +0.5)
         _mesh.addFace(
             spk::Mesh::VertexIndex(2, 0),
             spk::Mesh::VertexIndex(3, 1),
-            spk::Mesh::VertexIndex(7, 3),
-            spk::Mesh::VertexIndex(6, 2)
+            spk::Mesh::VertexIndex(6, 2),
+            spk::Mesh::VertexIndex(7, 3)
         );
 
-        // Bottom face (y = -0.5)
+        // // Bottom face (y = -0.5)
         _mesh.addFace(
             spk::Mesh::VertexIndex(0, 0),
             spk::Mesh::VertexIndex(4, 1),
-            spk::Mesh::VertexIndex(5, 3),
-            spk::Mesh::VertexIndex(1, 2)
+            spk::Mesh::VertexIndex(1, 2),
+            spk::Mesh::VertexIndex(5, 3)
         );
 
 		_material.setDiffuse(new spk::Image("resources/texture/diffuseTexture.png"));
@@ -98,6 +98,11 @@ public:
 	{
 
 	}
+
+	spk::Camera& camera()
+	{
+		return (_camera);
+	}
 };
 
 class GameEngineWidget : public spk::Widget
@@ -105,58 +110,107 @@ class GameEngineWidget : public spk::Widget
 private:
 	spk::GameEngine _engine;
 
-	Object _cube;
+	int _turning = 0;
+	Object _cube[4];
 	Player _player;
+
+	void _onGeometryChange() override
+	{
+		float ratio = static_cast<float>(geometry().size.x) / static_cast<float>(geometry().size.y);
+
+		_player.camera().setOrthographic(-5 * ratio, 5 * ratio, -5, 5, 1000.0, 0.1);
+		// _player.camera().setPerspective(90.0f, ratio, 1000.0f, 0.1f);
+	}
+
+	void _onKeyboardEvent(const spk::KeyboardEvent& p_event) override
+	{
+		if (p_event.type == spk::KeyboardEvent::Type::Press && p_event.key == spk::Keyboard::A)
+		{
+			_turning += -1;
+		}
+		if (p_event.type == spk::KeyboardEvent::Type::Press && p_event.key == spk::Keyboard::Z)
+		{
+			_turning += 0;
+		}
+		if (p_event.type == spk::KeyboardEvent::Type::Press && p_event.key == spk::Keyboard::E)
+		{
+			_turning += 1;
+		}
+	}
 
 	void _onPaintEvent(const spk::PaintEvent& p_event) override
 	{
-		DEBUG_LINE();
+		_player.getComponent<spk::Camera>().activate();
 		_engine.render();
-		DEBUG_LINE();
 	}
 
 	void _onUpdateEvent(const spk::UpdateEvent& p_event) override
 	{
+		if (p_event.deltaTime != 0)
+		{
+			float delta = 0.0180f * static_cast<float>(p_event.deltaTime) * _turning;
+			_player.transform().rotateAroundPoint(spk::Vector3(0, 0, 0), spk::Vector3(0, 1, 0), delta);
+			_player.transform().lookAt(spk::Vector3(0, 0, 0));
+
+			// _cube[1].transform().rotate(spk::Vector3(delta, 0, 0));
+			// _cube[2].transform().rotate(spk::Vector3(0, delta, 0));
+			// _cube[3].transform().rotate(spk::Vector3(0, 0, delta));
+		}
+
 		_engine.update(p_event.deltaTime);
+		p_event.requestPaint();
 	}
 
 public:
 	GameEngineWidget(const std::wstring& p_name, spk::SafePointer<spk::Widget> p_parent) :
 		spk::Widget(p_name, p_parent),
-		_cube(L"Cube"),
+		_cube{
+			Object(L"Cube A"),
+			Object(L"Cube B"),
+			Object(L"Cube C"),
+			Object(L"Cube D"),
+		},
 		_player()
 	{
-		DEBUG_LINE();
-		_player.transform().place(spk::Vector3(3, 3, 3));
-		_cube.transform().place(spk::Vector3(0, 0, 0));
-		_player.transform().lookAt(_cube.transform().position());
-		DEBUG_LINE();
+		_player.transform().place(spk::Vector3(5, 5, 5));
+
+		_cube[0].transform().place(spk::Vector3(0, 0, 0));
+		_cube[0].transform().setScale(spk::Vector3(1, 1, 1));
+
+		_cube[1].transform().place(spk::Vector3(1, 0, 0));
+		_cube[1].transform().setScale(spk::Vector3(2, 1, 1));
+
+		_cube[2].transform().place(spk::Vector3(0, 1, 0));
+		_cube[2].transform().setScale(spk::Vector3(1, 2, 1));
+
+		_cube[3].transform().place(spk::Vector3(0, 0, 1));
+		_cube[3].transform().setScale(spk::Vector3(1, 1, 2));
+		
+		_player.transform().lookAt(_cube[0].transform().position());
 
 		_engine.addEntity(&_player);
-		_engine.addEntity(&_cube);
-		DEBUG_LINE();
+		_engine.addEntity(&_cube[0]);
+		_engine.addEntity(&_cube[1]);
+		_engine.addEntity(&_cube[2]);
+		_engine.addEntity(&_cube[3]);
 
 		_player.setPriority(1000);
-		_cube.setPriority(0);
-		DEBUG_LINE();
+		_cube[0].setPriority(0);
+		_cube[1].setPriority(0);
+		_cube[2].setPriority(0);
+		_cube[3].setPriority(0);
 	}
 };
 
 int main()
 {
-		DEBUG_LINE();
     spk::GraphicalApplication app;
 
-		DEBUG_LINE();
     spk::SafePointer<spk::Window> win = app.createWindow(L"WorldOfElectron", spk::Geometry2D(0, 0, { 600, 600 }));
-		DEBUG_LINE();
-    win->setUpdateRate(0);
 
-		DEBUG_LINE();
 	GameEngineWidget gameEngineWidget = GameEngineWidget(L"GameEngineWidget", win->widget());
 	gameEngineWidget.setGeometry(win->geometry());
 	gameEngineWidget.activate();
-		DEBUG_LINE();
 
     return app.run();
 }
